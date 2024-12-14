3f7265e05c41f2f75f8e53710ca01badbb880247
Adam Santaniello <adam.santaniello@icloud.com>
2024-12-14 14:20:09 -0500

    ✨ Create devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@
 #                     rails_direct_uploads POST /rails/active_storage/direct_uploads(.:format)                                                    active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
